259d8f3307013ed46b042c46865d8a1c94f17958
重王泉 <1813114234@qq.com>
2019-08-11 21:44:27 +0800

    支持个人博客配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   // 页面标题
-  title: 'VuePress Demo',
+  title: 'jason 前端小院',
   // 网页描述
-  description: '一蓑烟雨、汪汪汪的个人站点',
+  description: '兴趣是最好的老师',
   head: [
     // 页面icon
-    ['link', { rel: 'icon', href: '/icon.png' }]
+    ['link', { rel: 'logo', href: '/logo.png' }]
   ],
   // 端口号
   port: 3000,
@@ -19,7 +19,7 @@ module.exports = {
     // 所有页面自动生成侧边栏
     sidebar: 'auto',
     // 仓库地址
-    repo: 'https://github.com/wangtunan/vuepress-blog-demo',
+    repo: 'https://github.com/zhongwangquan/vuePress-blog-jason',
     // 仓库链接label
     repoLabel: 'Github',
     // 编辑链接
@@ -28,18 +28,16 @@ module.exports = {
     editLinkText: '编辑此页',
     // 导航
     nav: [
-      { text: '前端面试之道',link: '/interview/'},
-      { text: 'JavaScript书籍', items: [
-          { text: '你不知道的JavaScript(上)', link: '/books/你不知道的javascript上'}, 
-          { text: '你不知道的JavaScript(中)', link: '/books/你不知道的javascript中'},
-          { text: '你不知道的JavaScript(下)', link: '/books/你不知道的javascript下'}
-        ]
-      },
-      { text: '数据结构和算法', link: '/algorithm/'},
-      { text: 'Vue.js', link: '/vue/'},
-      { text: 'Webpack',link: '/webpack/'},
-      { text: 'VuePress',link: '/vuepress/'},
-      { text: '基础配置功能',link: '/common/'}
+      { text: 'jason自学笔记', items: [
+        { text: '前端自学笔记-第一篇(0805)', link: '/learning/前端自学笔记1'}
+      ]
+    },
+      // { text: '前端面试之道',link: '/interview/'},
+      // { text: '数据结构和算法', link: '/algorithm/'},
+      // { text: 'Vue.js', link: '/vue/'},
+      // { text: 'Webpack',link: '/webpack/'},
+      // { text: 'VuePress',link: '/vuepress/'},
+      // { text: '基础配置功能',link: '/common/'}
   ]},
   configureWebpack: {
     resolve: {
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,14 @@
 ---
 home: true
 lang: zh-CN
-heroText: VuePress Demo
+heroText: jason 前端小院
 heroImage: /logo.jpg
 actionText: 开始 →
-actionLink: /interview/
+actionLink: /learning/
 features:
 - title: A Blog
-  details: 专注写作前端博客，记录日常所得。
+  details: 记录前端学习情况。
 - title: For Me
-  details: 故九万里，则风斯在下矣，而后乃今培风；背负青天，而莫之夭阏者，而后乃今将图南。
-- title: For Interview
-  details: 广州，已入坑ღ( ´･ᴗ･` )比心
-footer: Copyright © 2019-present wangtunan
+  details: 对技术探索充满了好奇。
+footer: Copyright © 2019-present jason
 ---
